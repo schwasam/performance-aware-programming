3a04136bea7544813238c4fa7ab6784db07fcfe0
schwasam <samuel.schwager@gmail.com>
2023-11-06 21:00:04 +0100

    clean up sim86

--- homework/sim86/src/Program.fs
+++ homework/sim86/src/Program.fs
@@ -5,24 +5,24 @@ let readFile path =
     File.ReadAllBytes path
     
 let clearDW (byte: byte) =
-    let step1 = byte &&& ~~~(0b00000001uy <<< 1) // clear D flag
-    let step2 = step1 &&& ~~~(0b00000001uy <<< 0) // clear W flag
-    step2
+    let cleared = byte &&& 0b11111100uy // clear D and W flag
+    cleared
     
 let decodeInstructions (bytes: byte array) =
-    let instruction = bytes[0]
-    match clearDW instruction with
+    let byte1 = bytes[0]
+    let byte2 = bytes[1]
+    let opcode = clearDW byte1
+    match opcode with
     | 0b10001000uy -> "mov"
     | _ -> failwith "unsupported instruction"
     
 [<EntryPoint>]
 let main args =
-    // let value = 0b11111111uy
-    // let cleared = clearDW value
-    // printfn $"Original: %B{value}"
-    // printfn $"Cleared:  %B{cleared}"
-    let all = readFile args[0] |> decodeInstructions
-    printfn $"Arguments:    %A{args}"
-    printfn $"Length:       %A{all.Length}"
-    printfn $"Instructions: %A{all}"
+    if args.Length < 1
+    then
+        printfn "Usage: sim86.exe <path>"
+    else
+        let all = readFile args[0] |> decodeInstructions
+        printfn $"Arguments:    %A{args}"
+        printfn $"Instructions: %A{all}"
     0
